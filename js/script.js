77d3e115aa18f228c76e2f0bfe38c8322ca0bb1f
brian <brianchen1988@gmail.com>
2015-06-17 23:57:13 -0700

    add search and fancy box

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(function () {
 	var searchField = $('#query');
 	var icon = $('#search-btn');
 
+	//Focus Event Handler
 	$(searchField).on('focus', function () {
 		$(this).animate({
 			width: '100%'
@@ -13,16 +14,201 @@ $(function () {
 		}, 400);
 	});
 
+	//Blur Event Handler
 	$(searchField).on('blur', function () {
 		if (searchField.val() == '') {
 			$(searchField).animate({
 				width: '45%'
 			}, 400, function () {});
-			
+
 			$(icon).animate({
 				right: '360px'
-			},400,function(){});
+			}, 400, function () {});
 		}
 	});
 
+	$('#search-form').submit(function (e) {
+		e.preventDefault();
+	});
 })
+
+
+function search() {
+	//clear results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	//Get form inputs
+	var q = $('#query').val();
+
+	//Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search", {
+			part: 'snippet, id',
+			q: q,
+			type: 'vedio',
+			key: 'AIzaSyC5Ol2z1Nu2lNNPrlD-qMjGMalJf1EWFH0'
+		},
+		function (data) {
+			var nextPageToken = data.nextPageToken;
+			var prevPageToken = data.prevPageToken;
+
+			console.log(data);
+
+			$.each(data.items, function (i, item) {
+				//Get Output
+				var output = getOutput(item);
+
+				//Display Results
+				$('#results').append(output);
+			});
+
+			var buttons = getButtons(prevPageToken, nextPageToken, q);
+
+			$('#buttons').append(buttons);
+		}
+	);
+}
+
+//Next page Function
+function nextPage() {
+	var token = $('#next-button').data('token');
+	var q = $('#next-button').data('query');
+	//clear results
+	$('#results').html('');
+	$('#buttons').html('');
+
+	//Get form inputs
+	q = $('#query').val();
+
+	//Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search", {
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type: 'vedio',
+			key: 'AIzaSyC5Ol2z1Nu2lNNPrlD-qMjGMalJf1EWFH0'
+		},
+		function (data) {
+			var nextPageToken = data.nextPageToken;
+			var prevPageToken = data.prevPageToken;
+
+			console.log(data);
+
+			$.each(data.items, function (i, item) {
+				//Get Output
+				var output = getOutput(item);
+
+				//Display Results
+				$('#results').append(output);
+			});
+
+			var buttons = getButtons(prevPageToken, nextPageToken, q);
+
+			$('#buttons').append(buttons);
+		}
+	);
+}
+
+function prevPage(){
+	var token = $('#prev-button').data('token');
+	var q = $('#prev-button').data('query');
+
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
+	
+	// Get Form Input
+	q = $('#query').val();
+	
+	// Run GET Request on API
+	$.get(
+		"https://www.googleapis.com/youtube/v3/search",{
+			part: 'snippet, id',
+			q: q,
+			pageToken: token,
+			type:'video',
+			key: 'AIzaSyCvk3NNMQASZgFkCNxIp9jH-l8O0PXhDUo'},
+			function(data){
+				var nextPageToken = data.nextPageToken;
+				var prevPageToken = data.prevPageToken;
+				
+				// Log Data
+				console.log(data);
+				
+				$.each(data.items, function(i, item){
+					// Get Output
+					var output = getOutput(item);
+					
+					// Display Results
+					$('#results').append(output);
+				});
+				
+				var buttons = getButtons(prevPageToken, nextPageToken);
+				
+				// Display Buttons
+				$('#buttons').append(buttons);
+			}
+	);
+}
+
+// Build Output
+function getOutput(item) {
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumb = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+
+	//Build Output String
+	var output = '<li>' +
+		'<div class = "list-left">' +
+		'<img src="' + thumb + '">' +
+		'</div>' +
+		'<div class = "list-right">' +
+		'<h3><a class="fancybox fancybox.iframe" href="http://youtube.com/embed/'+videoId+'">' + title + '</h3>' +
+		'<small>By <span class = "cTitle">' + channelTitle + '</span> on' + videoDate + '</small>' +
+		'<p>' + description + '</p>' +
+		'</div>' +
+		'</li>' +
+		'<div class="clearfix"></div>' +
+		'';
+
+	return output;
+}
+
+// Build Buttons
+
+//// Build the buttons
+//function getButtons(prevPageToken, nextPageToken){
+//	if(!prevPageToken){
+//		var btnoutput = '<div class="button-container">'+'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+//		'onclick="nextPage();">Next Page</button></div>';
+//	} else {
+//		var btnoutput = '<div class="button-container">'+
+//		'<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'"' +
+//		'onclick="prevPage();">Prev Page</button>' +
+//		'<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' +
+//		'onclick="nextPage();">Next Page</button></div>';
+//	}
+//	
+//	return btnoutput;
+//}
+
+function getButtons(prevPageToken, nextPageToken, q) {
+	if (!prevPageToken) {
+		var btnoutput = '<div class="button-container">' +
+			'<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-						query="' + q + '"' +
+			'onclick="nextPage();">Next Page</button></div>';
+	} else {
+		var btnoutput = '<div class="button-container">' +
+			'<button id="prev-button" class="paging-button" data-token="' + prevPageToken + '" data-						query="' + q + '"' +
+			'onclick="prevPage();">Previous Page</button>' +
+			'<button id="next-button" class="paging-button" data-token="' + nextPageToken + '" data-						query="' + q + '"' +
+			'onclick="nextPage();">Next Page</button></div>';
+	}
+
+	return btnoutput;
+}
